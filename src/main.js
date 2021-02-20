fecfefa09e2e87f71d581fc474597690b2f11f67
Scott Ian <sim2487@gmail.com>
2021-02-19 18:59:35 -0800

    Initial setup.

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import axios from 'axios';
 
-createApp(App)
+const app = createApp(App)
   .use(store)
-  .use(router)
-  .mount("#app");
+  .use(router);
+
+app.config.globalProperties.axios = axios;
+app.mount('#app');
