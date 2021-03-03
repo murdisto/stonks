89400b4ddedb9a665534632dece0ed277aaba9b7
Scott Ian <sim2487@gmail.com>
2021-03-02 20:03:48 -0800

    Followed stocks displaying in MyStonk component. Unfollow button removes stock from list.

--- src/components/MyStonks.vue
+++ src/components/MyStonks.vue
@@ -2,35 +2,63 @@
   <div>
     <h1>MYSTONKS</h1>
     <div>
-      <div v-for="symbol in symbols" :key="symbol.symbol">
-        {{ symbol }}
-        <!-- <button
-          v-if="compareStonks(symbol.symbol)"
-          @click="followStonk(symbol.symbol)"
+      <div v-for="stonk in stonks" :key="stonk.symbol">
+        {{ stonk.name }} {{ stonk.price }}
+        <button
+          v-if="compareStonks(stonk.symbol)"
+          @click="followStonk(stonk.symbol)"
         >
           +follow
         </button>
-        <button v-else @click="unfollowStonk(symbol.symbol)">-unfollow</button> -->
+        <button v-else @click="unfollowStonk(stonk.symbol, index)">
+          -unfollow
+        </button>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 import { mapState } from "vuex";
+
+const API_KEY = process.env.VUE_APP_FNP_API_KEY;
 
 export default {
   data() {
     return {
       symbols: [],
+      stonks: [],
     };
   },
   created() {
     this.symbols = this.userProfile.stonks;
-    console.log("this.symbols: ", this.symbols);
+    const symbolsString = this.symbols.toString();
+    const BASE_URL = "https://financialmodelingprep.com/api/v3/";
+    const apiURL = `${BASE_URL}quote/${symbolsString}?apikey=${API_KEY}`;
+
+    axios
+      .get(apiURL)
+      .then((res) => {
+        this.stonks = [...res.data];
+        console.log("stonks: ", this.stonks);
+      })
+      .catch((err) => console.error(err));
   },
   computed: {
     ...mapState(["userProfile"]),
+  },
+  methods: {
+    followStonk(symbol) {
+      this.$store.dispatch("addFollowedStonk", symbol);
+    },
+    unfollowStonk(symbol, index) {
+      this.$store.dispatch("removeFollowedStonk", symbol);
+      this.stonks.splice(index, 1);
+    },
+    compareStonks(symbol) {
+      return !this.userProfile.stonks.includes(symbol);
+    },
   },
 };
 </script>
